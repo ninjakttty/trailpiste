93985423bbfb7df578c2d8a059e2ba26c46990ae
Yuri A. Parsons <yuri@ninjakitty.net>
2017-10-02 09:40:32 +0200

    add apollo libraries

--- pages/index.js
+++ pages/index.js
@@ -1,1 +1,21 @@
-export default () => <div>Welcome to trailpiste!</div>;
+import { ApolloProvider, createNetworkInterface, ApolloClient } from 'react-apollo';
+import List from '../components/List';
+import Create from '../components/Create';
+
+const networkInterface = createNetworkInterface({
+  // __SIMPLE_API_ENDPOINT__ looks similar to: `https://api.graph.cool/simple/v1/<PROJECT_ID>`
+  uri: 'https://api.graph.cool/simple/v1/cj89un4bt0860012227gdvade',
+});
+
+const client = new ApolloClient({ networkInterface });
+
+export default () => (
+  <ApolloProvider client={client}>
+    <div>
+      <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.2.2/semantic.min.css" />
+      <div>Welcome to trailpiste!</div>
+      <List />
+      <Create />
+    </div>
+  </ApolloProvider>
+);
